b9348b3c68860c91e9fe50a4aa8bab1d7de48a6d
farid324rahvari-boop <farid324.rahvari@gmail.com>
2026-08-27 10:58:49 +0330

    Add files via upload

--- routes/auto-alert.js
+++ routes/auto-alert.js
@@ -1,1 +1,6 @@
-
+const express = require('express');
+const router = express.Router();
+const { checkAndAlert, status } = require('../services/auto-alert');
+router.get('/status', (req,res)=>res.json(status()));
+router.post('/check', async (req,res)=>res.json(await checkAndAlert()));
+module.exports = router;
